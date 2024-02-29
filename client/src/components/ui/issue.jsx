a4c80a2c82ed0392dab80a70feaa83011b4a50d4
Jared Elliott <bootcamp@jaredclt.com>
2024-02-29 18:55:04 -0500

    class to className

--- client/src/components/ui/issue.jsx
+++ client/src/components/ui/issue.jsx
@@ -1,17 +1,17 @@
 export default function issue( { issues } ) {
 
     return (
-        <div class="container" id="gh-repo-issues">
-        <ul class="list-group">
+        <div className="container" id="gh-repo-issues">
+        <ul className="list-group">
             {issues.map((data, index) => 
                 <li key={index} className="list-group-item d-flex align-items-center">
-                <div class="badge text-bg-info rounded-pill">5</div>
-                    <div class="text-start mx-2">
-                        <h5 class="m-0">{data.title}</h5>
+                <div className="badge text-bg-info rounded-pill">5</div>
+                    <div className="text-start mx-2">
+                        <h5 className="m-0">{data.title}</h5>
                         <small>{data.description}</small>
                     </div>
-                    <a href="#" class="ms-auto btn btn-success">{data.bounty} <i class="fa-solid fa-hand-holding-dollar"></i></a>
-                    <a href="#" class="mx-2 btn btn-dark" title="Claim Bounty"><i class="fa-solid fa-virus-slash"></i></a>
+                    <a href="#" className="ms-auto btn btn-success">{data.bounty} <i className="fa-solid fa-hand-holding-dollar"></i></a>
+                    <a href="#" className="mx-2 btn btn-dark" title="Claim Bounty"><i className="fa-solid fa-virus-slash"></i></a>
                 </li>
             )}
         </ul>
