e56f8ed5e5d17a644ed240c503fd54745da03a73
Traspo <nyaspo8@gmail.com>
2024-03-01 23:18:11 -0500

    styled login page and minor changes to footer and contact

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -30,40 +30,47 @@ function Login(props) {
   };
 
   return (
-    <div className="container my-1">
+    <div className="container mb-4">
       <Link to="/signup">← Go to Signup</Link>
 
-      <h2>Login</h2>
-      <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email address:</label>
-          <input
-            placeholder="youremail@test.com"
-            name="email"
-            type="email"
-            id="email"
-            onChange={handleChange}
-          />
+      <h2 className='text-success h1-responsive font-weight-bold text-center my-4'>Login</h2>
+
+      <div className="row">
+        <div className=" mb-md-0 mb-5">
+          <form onSubmit={handleFormSubmit} id="login-form" name="login-form" action="tbd" method="POST">
+
+            <div className="row my-2">
+              <div className="">
+                <div className="md-form mb-0">
+                  <label htmlFor="email" className="">Email address</label>
+                  <input placeholder="youremail@test.com" type="email" id="email" name="email" className="form-control" onChange={handleChange} />
+                </div>
+              </div>
+            </div>
+
+            <div className="row my-4">
+              <div className="">
+                <div className="md-form">
+                  <label htmlFor="pwd">Password</label>
+                  <input placeholder="******" type="password" id="pwd" name="password" rows="2" className="form-control" onChange={handleChange}></input>
+                </div>
+              </div>
+            </div>
+
+            {error ? (
+              <div>
+                <p className="error-text">The provided credentials are incorrect</p>
+              </div>
+            ) : null}
+
+            <div className="text-center text-md-left mt-3">
+              <a className="btn btn-success" >Submit</a>
+              {/* onClick="document.getElementById('login-form').submit();" */}
+            </div>
+            <div className="status"></div>
+          </form>
         </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
-          <input
-            placeholder="******"
-            name="password"
-            type="password"
-            id="pwd"
-            onChange={handleChange}
-          />
-        </div>
-        {error ? (
-          <div>
-            <p className="error-text">The provided credentials are incorrect</p>
-          </div>
-        ) : null}
-        <div className="flex-row flex-end">
-          <button type="submit">Submit</button>
-        </div>
-      </form>
+      </div>
     </div>
   );
 }
