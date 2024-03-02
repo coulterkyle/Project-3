a8b6b88f59152d384cd5511531bab06238c19a4e
Jared Elliott <bootcamp@jaredclt.com>
2024-03-02 16:53:31 -0500

    Changes to navigation into Navbar object

--- client/src/components/navigation.jsx
+++ client/src/components/navigation.jsx
@@ -7,57 +7,44 @@ function Nav() {
   function showNavigation() {
     if (Auth.loggedIn()) {
       return (
-        <div className="container-fluid">
-        <a className="navbar-brand" href="#">GitPrize</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-md-0">
-              <li className="nav-item">
-                <Link className="nav-link" to="/">Home</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/project/coulterkyle">Projects</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="#">About</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="#">Contact</Link>
-              </li>
-            </ul>
-
-            <ul className="navbar-nav me-auto mb-2 mb-md-0">
-              <li className="mx-1">
-                {/* this is not using the Link component to logout or user and then refresh the application to the start */}
-                <a href="/" onClick={() => Auth.logout()}>
-                  Logout
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      );
-    } else {
-      return (
         <Navbar
+          auth={true}
           links={[
             <Link key={1} className="nav-link" to="/">
               Home
             </Link>,
-            <Link key={1} className="nav-link" to="/project/coulterkyle">
+            <Link key={2} className="nav-link" to="/project/coulterkyle">
               Projects
             </Link>,
-            <Link key={1} className="nav-link" to="/about">
+            <Link key={3} className="nav-link" to="/about">
               About
             </Link>,
-            <Link key={1} className="nav-link" to="/contact">
+            <Link key={4} className="nav-link" to="/contact">
               Contact
             </Link>
-
           ]}
         />
+      );
+    } else {
+      return (
+        <Navbar
+          auth={false}
+          links={[
+            <Link key={1} className="nav-link" to="/">
+              Home
+            </Link>,
+            <Link key={2} className="nav-link" to="/project/coulterkyle">
+              Projects
+            </Link>,
+            <Link key={3} className="nav-link" to="/about">
+              About
+            </Link>,
+            <Link key={4} className="nav-link" to="/contact">
+              Contact
+            </Link>
+          ]}
+        />
+
       );
     }
   }
