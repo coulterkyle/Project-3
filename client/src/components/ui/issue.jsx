620f6b5a76448d96ee2f0c0abe15c87bcd5f0f0e
Traspo <nyaspo8@gmail.com>
2024-03-01 21:00:38 -0500

    Adding starter code for modal

--- client/src/components/ui/issue.jsx
+++ client/src/components/ui/issue.jsx
@@ -2,19 +2,29 @@ export default function issue({ issues }) {
 
     return (
         <div className="container" id="gh-repo-issues">
-        <ul className="list-group">
-            {issues.map((data, index) => 
-                <li key={index} className="list-group-item d-flex align-items-center">
-                <div className="badge text-bg-info rounded-pill">{data.vote}</div>
-                    <div className="text-start mx-2">
-                        <h5 className="m-0">{data.title}</h5>
-                        <small>{data.description}</small>
-                    </div>
-                    <a href="#" data-id={data.id} className="ms-auto btn btn-success">{data.bounty} <i className="fa-solid fa-hand-holding-dollar"></i></a>
-                    <a href="#" className="mx-2 btn btn-dark" title="Claim Bounty"><i className="fa-solid fa-virus-slash"></i></a>
-                </li>
-            )}
-        </ul>
+            <ul className="list-group">
+                {issues.map((data, index) =>
+                    <li key={index} className="list-group-item d-flex align-items-center">
+                        <div className="badge text-bg-info rounded-pill">{data.vote}</div>
+                        <div className="text-start mx-2">
+                            <h5 className="m-0">{data.title}</h5>
+                            <small>{data.description}</small>
+                        </div>
+                        <button href="#"
+                            data-id={data.id}
+                            className="ms-auto btn btn-success"
+                            type="button"
+                            data-toggle="modal"
+                            data-target="#exampleModal"
+                            data-whatever={data.title}
+                            data-body={data.body}>{data.bounty}
+                            <i className="fa-solid fa-hand-holding-dollar"></i>
+                        </button>
+                        <a href="#" data-id={data.id} className="ms-auto btn btn-success">{data.bounty} <i className="fa-solid fa-hand-holding-dollar"></i></a>
+                        <a href="#" className="mx-2 btn btn-dark" title="Claim Bounty"><i className="fa-solid fa-virus-slash"></i></a>
+                    </li>
+                )}
+            </ul>
         </div>
     );
 }
---
+++ client/src/components/ui/modal.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+const ModalComponent = () => {
+  const [recipient, setRecipient] = useState('');
+
+  const handleModalShow = (event) => {
+    const button = event.relatedTarget;
+    const recipient = button.getAttribute('data-whatever');
+
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+
+    // Update the modal's content
+    setRecipient(recipient);
+  };
+
+  return (
+    <div>
+      <div id="exampleModal" onClick={handleModalShow}>
+        Open Modal
+      </div>
+      <div className="modal">
+        <div className="modal-title">New message to {recipient}</div>
+        <div className="modal-body">
+          <input type="text" value={recipient} readOnly />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ModalComponent;
