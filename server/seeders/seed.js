5fdef16b73c37988076b08a3f1e596ef90630d3f
Jared Elliott <bootcamp@jaredclt.com>
2024-03-01 21:25:34 -0500

    Created seed data to create users

---
+++ server/seeders/seed.js
@@ -0,0 +1,12 @@
+const db = require('../config/connection');
+const { User } = require('../models');
+const userSeeds = require('./userSeed.json');
+const cleanDB = require('./cleanDB')
+
+db.once('open', async () => {
+  await cleanDB('User', 'users');
+  await User.create(userSeeds);
+
+  console.log('all done!');
+  process.exit(0);
+});
