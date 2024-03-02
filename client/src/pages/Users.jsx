81f9d76a17a45c03b23350390b9baaccd9f00fa5
Jared Elliott <bootcamp@jaredclt.com>
2024-03-02 12:49:49 -0500

    Styled user that gets displayed

--- client/src/pages/Users.jsx
+++ client/src/pages/Users.jsx
@@ -1,4 +1,5 @@
 import { useQuery } from '@apollo/client';
+import { Link } from 'react-router-dom';
 import { QUERY_USERS } from '../utils/queries';
 
 const listUsers = () => {
@@ -10,8 +11,18 @@ const listUsers = () => {
   if (loading) return <div className="container">Loading, please wait...</div>;
   
   return (
+    // change to a ui component once we know why we can't list all users
     <div className="container">
-      {userData.githubUsername}
+      <ul className="list-group">
+        <li className="list-group-item d-flex align-items-center">
+          <div className="badge text-bg-info rounded-pill"></div>
+            <div className="text-start mx-2">
+                <h5 className="m-0">{userData.firstName} {userData.lastName}</h5>
+                <small>{userData.githubUsername}</small>
+            </div>
+            <Link to={userData.githubUsername} className="ms-auto btn btn-success">View</Link>
+        </li>
+      </ul>
     </div>
   );
 };
