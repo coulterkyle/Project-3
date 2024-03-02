cd0d7c624f75646c559a4cb5f78fb43ae68200e4
kyle coulter <152663440+coulterkyle@users.noreply.github.com>
2024-03-02 14:00:55 -0500

    removed unnecessary models, reconfiged index.js

--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,5 @@
 const User = require('./User');
 const Issue = require('./Issue');
-const Vote = require('./Vote');
-const Bounty = require('./Bounty');
 
 
-module.exports = { User, Issue, Vote, Bounty };
+module.exports = { User, Issue };
