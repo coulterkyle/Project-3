488533f59be1bb85fc56bd021977a1ef027fff7f
Traspo <nyaspo8@gmail.com>
2024-03-02 20:54:43 -0500

    Small style changes and added About page

--- client/src/components/ui/repo.jsx
+++ client/src/components/ui/repo.jsx
@@ -7,12 +7,12 @@ export default function repo({ repos }) {
         <ul className="list-group">
             {repos.map((data, index) => 
                 <li key={index} className="list-group-item d-flex align-items-center">
-                <div className="badge text-bg-info rounded-pill">{data.open_issues}</div>
+                <div className="badge text-bg-success rounded-pill">{data.open_issues}</div>
                     <div className="text-start mx-2">
                         <h5 className="m-0">{data.name}</h5>
                         <small>{data.description}</small>
                     </div>
-                    <Link to={data.name} data-id={data.id} className="ms-auto btn btn-info">View Issues</Link>
+                    <Link to={data.name} data-id={data.id} className="ms-auto btn btn-success">View Issues</Link>
                 </li>
             )}
         </ul>
