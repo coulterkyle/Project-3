871bed3570a3b0ffbf9bb42f06e076f35f21ec86
Jared Elliott <bootcamp@jaredclt.com>
2024-03-04 18:51:47 -0500

    Added decoding information to component.

--- client/src/pages/Success.jsx
+++ client/src/pages/Success.jsx
@@ -1,25 +1,46 @@
 import success from '../assets/success.png'
-import background from '../assets/backgroundImg3.png'
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import { decodeCHECKOUT } from '../utils/mutations';
 
 const Success = () => {
-    return (
-      <div  style={{ backgroundImage: `url(${background})` }}>
-        <div className="container my-5 bg-dark">
-          <div className="row p-4  pe-lg-0  align-items-center rounded shadow-lg ">
+  const { sessionId } = useParams()
   
-            <div className="col-lg-2 shadow-lg">
-              <img className="rounded-lg-3" src={success} alt="" width="300" />
+  const [bTotal, setTotal] = useState(0);
+  const [decodeMe, { loading, error }] = useMutation(decodeCHECKOUT);
+
+  const decodeStripe = async () => {
+    const decodeData = await decodeMe({variables: {sessionId: sessionId}})
+    return decodeData.data.decodeStripe.total
+  }
+
+  useEffect(() => {    
+    const dataDecode = decodeStripe()
+    dataDecode.then((res) => {
+      console.log('Total', res)
+      setTotal(res/100) 
+    });
+    }, []);
+
+    if (loading) return <div className="container">Loading, please wait...</div>;
+    if (error) return <Error />
+
+    return (
+      <div className="container">
+          <div className="row p-4 align-items-center">
+  
+            <div className="col-md-2 shadow-lg">
+              <img className="rounded-lg-3" src={success} alt="" width="250" />
             </div>
   
             <div className="col-lg-7 p-3 p-lg-5 mx-auto pt-lg-3">
-              <h1 className="display-4 fw-bold lh-1 text-body-success text-success">BOUNTY RECEIVED</h1>
-              <p className="lead">Thank you for your bounty for XX issue.
-                <br></br>
-                <br></br>
-                Now we wait...</p>
+              <h2 className="fw-bold text-success">BOUNTY RECEIVED</h2>
+              <p className="lead">Thank you for your bounty for XX issue.</p>
+              <p>Other Information Will Load Here...</p>
+              <p id="stripeTotal">Total: ${bTotal}</p>
             </div>
           </div>
-        </div>
       </div>
     );
   };
