5fd0c5d443d459d13700bd22ab6f74e787000d63
Traspo <nyaspo8@gmail.com>
2024-03-04 11:54:27 -0500

    Adjusted text color

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -21,7 +21,7 @@ const Profile = () => {
             <div className="accordion" id="accordionExample">
                 <div className="accordion-item">
                     <h2 className="accordion-header" id="headingOne">
-                        <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" fdprocessedid="zc7rwf">
+                        <button className="accordion-button text-success" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" fdprocessedid="zc7rwf">
                             Bounties Offered
                         </button>
                     </h2>
@@ -38,7 +38,7 @@ const Profile = () => {
                 </div>
                 <div className="accordion-item">
                     <h2 className="accordion-header" id="headingTwo">
-                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo" fdprocessedid="tbazrz">
+                        <button className="accordion-button collapsed text-success" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo" fdprocessedid="tbazrz">
                             Bounties Claimed
                         </button>
                     </h2>
