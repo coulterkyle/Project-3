97221a67eb1ec7dddc5bc9d14bf468c6cc8c123e
Traspo <nyaspo8@gmail.com>
2024-03-04 11:14:53 -0500

    Name pulling to intro of page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,18 +1,46 @@
-import IssueList from '../components/issueList';
-import { useParams, Link } from 'react-router-dom';
+// import IssueList from '../components/issueList';
+import { useParams, Navigate } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+// import { useState } from "react";
+import { QUERY_ME } from '../utils/queries';
+// import ReactDOM from "react-dom/client";
+// import Auth from '../utils/auth';
 
 const Profile = () => {
 
-    const { firstName, lastName } = useParams()
+    const { username: userParam } = useParams();
+
+    const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
+        variables: { username: userParam },
+    });
+
+    const user = data?.me || data?.user || {};
+    // navigate to personal profile page if username is yours
+    // if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+    //     return <Navigate to="/profile" />;
+    // }
+
+    // if (loading) {
+    //     return <div>Loading...</div>;
+    // }
+
+    // if (!user?.username) {
+    //     return (
+    //         <h4>
+    //             You need to be logged in to see this. Use the navigation links above to
+    //             sign up or log in!
+    //         </h4>
+    //     );
+    // }
 
     return (
         <div className="container">
-            <h1>Welcome NAME {firstName} {lastName}</h1>
+            <h1>{`Welcome ${user.firstName} ${user.lastName}`}</h1>
             <div class="card text-center">
                 <div class="card-header">
                     <ul class="nav nav-tabs card-header-tabs">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="true" href="#">Bounties Set</a>
+                            <a class="nav-link active" aria-current="true" href="#">Bounties Offered</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">Bounties Claimed</a>
