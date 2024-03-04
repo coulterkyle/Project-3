780cb05bae8056a0eecb927e7c67a4ebe92e582b
Jared Elliott <bootcamp@jaredclt.com>
2024-03-03 23:06:24 -0500

    Added onClick to set modal form values for stripe.

--- client/src/components/ui/issue.jsx
+++ client/src/components/ui/issue.jsx
@@ -2,6 +2,14 @@ import Modal from './modal';
 
 export default function issue({ issues }) {
 
+const setFormValues = (event) => {
+    const bountyValues = document.getElementById(event.currentTarget.id)
+    console.log(bountyValues)
+    document.getElementById('bountyAmount').value = 10
+    document.getElementById('bountyId').text = bountyValues.id
+    document.getElementById('bountyName').text = bountyValues.whatever
+}
+
     return (
         <div className="container" id="gh-repo-issues">
             <ul className="list-group">
@@ -10,16 +18,19 @@ export default function issue({ issues }) {
                         <div className="badge text-bg-info rounded-pill">{data.vote}</div>
                         <div className="text-start mx-2">
                             <h5 className="m-0">{data.title}</h5>
-                            <small>{data.description}</small>
+                            <small className="text-muted">GitHub Issue #{data.number} - Created by {data.user.login}</small>
                         </div>
                         <button
                             data-bs-id={data.id}
                             className="ms-auto btn btn-success"
+                            id={data.id}
                             type="button"
                             data-bs-toggle="modal"
                             data-bs-target="#exampleModal"
                             data-bs-whatever={data.title}
-                            data-bs-body={data.body}>
+                            data-bs-body={data.body}
+                            onClick={setFormValues}   
+                        >
                                 {data.bounty}
                             <i className="fa-solid fa-hand-holding-dollar"></i>
                         </button>
