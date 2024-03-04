96273e25a86452752457018d4c06f5c8fb00c2b7
Traspo <nyaspo8@gmail.com>
2024-03-03 20:46:27 -0500

    basic layout for bountboard page in place

--- client/src/pages/BountyBoard.jsx
+++ client/src/pages/BountyBoard.jsx
@@ -1,4 +1,4 @@
-const BountyBoard = () => {
+export default function BountyBoard({ issues }) {
     return (
         <div>
             {/* Top Bounties */}
@@ -67,11 +67,33 @@ const BountyBoard = () => {
             </div>
 
             {/* All other Bounties */}
-
+            {/* <div className="container" id="gh-repo-issues">
+            <ul className="list-group">
+                {/* {issues.map((data, index) => */}
+                    {/* <li key={index} className="list-group-item d-flex align-items-center">
+                        <div className="badge text-bg-info rounded-pill">{data.vote}</div>
+                        <div className="text-start mx-2">
+                            <h5 className="m-0">{data.title}</h5>
+                            <small>{data.description}</small>
+                        </div> */}
+                        {/* <button
+                            data-bs-id={data.id}
+                            className="ms-auto btn btn-success"
+                            type="button"
+                            data-bs-toggle="modal"
+                            data-bs-target="#exampleModal"
+                            data-bs-whatever={data.title}
+                            data-bs-body={data.body}>
+                                {data.bounty}
+                            <i className="fa-solid fa-hand-holding-dollar"></i>
+                        </button>
+                        <a href="#" className="mx-2 btn btn-dark" title="Claim Bounty"><i className="fa-solid fa-virus-slash"></i></a>
+                    </li>
+                {/* )} */}
+            {/* </ul> */}
+        {/* </div> */}
 
 
         </div>
     );
 };
-
-export default BountyBoard;
