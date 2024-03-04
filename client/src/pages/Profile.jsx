70f0814f8ca71166f75bcb3f3a019ceda43ca970
Traspo <nyaspo8@gmail.com>
2024-03-04 10:57:10 -0500

    Updated styling and links for profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -3,21 +3,30 @@ import { useParams, Link } from 'react-router-dom';
 
 const Profile = () => {
 
-  const { firstName, lastName } = useParams()
+    const { firstName, lastName } = useParams()
 
     return (
-      <div className="container">
-        <h1>Welcome {firstName} {lastName}</h1>
-        <div>
-            <h2>Bounties Set</h2>
-            
+        <div className="container">
+            <h1>Welcome NAME {firstName} {lastName}</h1>
+            <div class="card text-center">
+                <div class="card-header">
+                    <ul class="nav nav-tabs card-header-tabs">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="true" href="#">Bounties Set</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Bounties Claimed</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Special title treatment</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                </div>
+            </div>
+
         </div>
-        <div>
-            <h2>Bounties Claimed</h2>
-        </div>
-        
-      </div>
     )
-  };
-  
-  export default Profile;
+};
+
+export default Profile;
