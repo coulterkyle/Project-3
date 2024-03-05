be3aa9114692567f17f8b9cba2223bd6ce0388a0
Jared Elliott <bootcamp@jaredclt.com>
2024-03-05 11:30:16 -0500

    Loop through bounties to see if it matches current issue.

--- client/src/components/ui/issue.jsx
+++ client/src/components/ui/issue.jsx
@@ -1,4 +1,6 @@
 import Modal from './modal';
+import { useQuery } from '@apollo/client'; 
+import { QUERY_BOUNTIES } from '../../utils/queries';
 
 export default function issue({ issues }) {
 
@@ -10,10 +12,30 @@ const setFormValues = (event) => {
     document.getElementById('name').value = bountyValues.dataset.title
 }
 
+    const { loading, data } = useQuery(QUERY_BOUNTIES);
+    if(loading) return <div className="container">Loading bounties & issues, please wait...</div>
+    const bountyData = data.issues;
+    const bountyIds = data.issues.map((bounty, index) => {
+        return bounty.issueId
+    })
+
+
+    console.log('IDs:', bountyIds)
+    console.log('data:', bountyData)
+
+    issues.map((data, index) => {
+        const dataString = data.id.toString()
+        console.log("ID Check:", bountyIds.includes(dataString))
+        if(bountyIds.includes(dataString)) console.log(bountyData[index].bounty)
+            else console.log('nay')
+    })
+
+
     return (
         <div className="container" id="gh-repo-issues">
             <ul className="list-group">
                 {issues.map((data, index) =>
+
                     <li key={index} className="list-group-item d-flex align-items-center">
                         <div className="badge text-bg-info rounded-pill">{data.vote}</div>
                         <div className="text-start mx-2">
