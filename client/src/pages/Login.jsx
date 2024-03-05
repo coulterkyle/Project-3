72ab8634fb586d01b563849a4e1913378cf335f2
dcast217 <151418582+dcast217@users.noreply.github.com>
2024-03-05 13:57:00 -0500

    added user id to localstorage

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -15,6 +15,7 @@ function Login(props) {
         variables: { email: formState.email, password: formState.password },
       });
       const token = mutationResponse.data.login.token;
+      localStorage.setItem('userId', mutationResponse.data.login.user._id)
       Auth.login(token);
     } catch (e) {
       console.log(e);
