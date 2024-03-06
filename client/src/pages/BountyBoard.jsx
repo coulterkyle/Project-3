42aed6c522f86a0373a944ad88717f41d3d07695
dcast217 <151418582+dcast217@users.noreply.github.com>
2024-03-05 21:44:25 -0500

    updated bounty board

--- client/src/pages/BountyBoard.jsx
+++ client/src/pages/BountyBoard.jsx
@@ -1,28 +1,26 @@
 import { Navigate } from 'react-router-dom'
-import {QUERY_ISSUES} from '../utils/queries'
+import { QUERY_ISSUES } from '../utils/queries'
 import { useQuery } from '@apollo/client'
 import Auth from '../utils/auth'
 
 
-
-
 export default function BountyBoard({ issues }) {
 
-    if(!Auth.loggedIn()) return <Navigate to="/login" />
-    const {loading, data} = useQuery(QUERY_ISSUES)
+    if (!Auth.loggedIn()) return <Navigate to="/login" />
+    const { loading, data } = useQuery(QUERY_ISSUES)
     console.log(data)
 
-    if(loading) return <div className="container">Loading bounties, please wait...</div>
-    const issue = data?.issues|| [] 
+    if (loading) return <div className="container">Loading bounties, please wait...</div>
+    const issue = data?.issues || []
     console.log(issue, "issue")
     // console.info('MyBounties:', data.QUERY_ISSUES)
 
     return (
         <div>
             {/* Top Bounties */}
-            <div className="container col-xxl-8 px-4 py-5" >
+            <div className="container col-xxl-8 py-2" >
                 <div className="pricing-header p-3 pb-md-4 mx-auto text-center">
-                    <h1 className="display-4 fw-normal text-success">Top Bounties</h1>
+                    <h2 className="fw-bold text-success">Top Bounties</h2>
                 </div>
                 <div className="row row-cols-1 row-cols-md-3 mb-3 text-center">
                     <div className="col">
@@ -30,13 +28,13 @@ export default function BountyBoard({ issues }) {
                             {/* Links the whole box to that particular issue */}
                             <a style={{ textDecoration: 'none' }} href="http://localhost:3000/project/coulterkyle/Project-3">
                                 <div className="card-header py-3">
-                                    <h4 className="my-0 fw-normal">Kyle's Issue Something</h4>
+                                    <h4 className="my-0 fw-normal">coulterkyle Project 3</h4>
                                 </div>
                                 <div className="card-body">
-                                    <h1 className="card-title pricing-card-title text-success">$1,000,000</h1>
+                                    <h1 className="card-title pricing-card-title text-success">$1,000</h1>
                                     <ul className="list-unstyled mt-3 mb-4">
-                                        <li>Brief description</li>
-                                        <li>Blah, Blah, something, something...</li>
+                                        <li>Help Bounty Issuers Claim Bounty</li>
+                                        <li>Send notification and review code</li>
                                     </ul>
                                     <button type="button" className="w-100 btn btn-lg btn-outline-primary btn-success">CLAIM BOUNTY</button>
                                 </div>
@@ -49,13 +47,13 @@ export default function BountyBoard({ issues }) {
                             {/* Links the whole box to that particular issue */}
                             <a style={{ textDecoration: 'none' }} href="http://localhost:3000/project/coulterkyle/Project-3">
                                 <div className="card-header py-3">
-                                    <h4 className="my-0 fw-normal">Kyle's Issue Something</h4>
+                                    <h4 className="my-0 fw-normal">dcast217 Project 3</h4>
                                 </div>
                                 <div className="card-body">
-                                    <h1 className="card-title pricing-card-title text-success">$1,000,000</h1>
+                                    <h1 className="card-title pricing-card-title text-success">$500</h1>
                                     <ul className="list-unstyled mt-3 mb-4">
-                                        <li>Brief description</li>
-                                        <li>Blah, Blah, something, something...</li>
+                                        <li>Ability to Upvote Issues</li>
+                                        <li>Improve visibility of priority issues</li>
                                     </ul>
                                     <button type="button" className="w-100 btn btn-lg btn-outline-primary btn-success">CLAIM BOUNTY</button>
                                 </div>
@@ -68,13 +66,13 @@ export default function BountyBoard({ issues }) {
                             {/* Links the whole box to that particular issue */}
                             <a style={{ textDecoration: 'none' }} href="http://localhost:3000/project/coulterkyle/Project-3">
                                 <div className="card-header py-3">
-                                    <h4 className="my-0 fw-normal">Kyle's Issue Something</h4>
+                                    <h4 className="my-0 fw-normal">NatalieYaspo Project 3</h4>
                                 </div>
                                 <div className="card-body">
-                                    <h1 className="card-title pricing-card-title text-success">$1,000,000</h1>
+                                    <h1 className="card-title pricing-card-title text-success">$750</h1>
                                     <ul className="list-unstyled mt-3 mb-4">
-                                        <li>Brief description</li>
-                                        <li>Blah, Blah, something, something...</li>
+                                        <li>Automate payout</li>
+                                        <li>Allow our users to get paid!</li>
                                     </ul>
                                     <button type="button" className="w-100 btn btn-lg btn-outline-primary btn-success">CLAIM BOUNTY</button>
                                 </div>
@@ -86,22 +84,22 @@ export default function BountyBoard({ issues }) {
 
             {/* All other Bounties */}
             <div className="container mb-4 success" id="gh-repo-issues">
-            <ul className="list-group">
-                {issue.map((data, index) => 
-                    <li key={index} className="list-group-item d-flex align-items-center">
-                        <div className="text-start mx-2">
-                            <h5 className="m-0">{data.title}</h5>
-                            <small>{data.description}</small>
-                        </div>
-                        <button className="ms-auto btn btn-success" type="button">
-                            {data.bounty}
-                            <i className="fa-solid fa-hand-holding-dollar"></i>
-                        </button>
-                        <a href="#" className="mx-2 btn btn-dark" title="Claim Bounty"><i className="fa-solid fa-virus-slash"></i></a>
-                    </li>
-                )}
-            </ul>
-        </div>
+                <ul className="list-group">
+                    {issue.map((data, index) =>
+                        <li key={index} className="list-group-item d-flex align-items-center">
+                            <div className="text-start mx-2">
+                                <h5 className="m-0">{data.title}</h5>
+                                <small>{data.description}</small>
+                            </div>
+                            <button className="ms-auto btn btn-success" type="button">
+                                {data.bounty}
+                                <i className="fa-solid fa-hand-holding-dollar"></i>
+                            </button>
+                            <a href="#" className="mx-2 btn btn-dark" title="Claim Bounty"><i className="fa-solid fa-virus-slash"></i></a>
+                        </li>
+                    )}
+                </ul>
+            </div>
 
 
         </div>
