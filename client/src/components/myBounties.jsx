144e9307b3eb985e10ae50cfc9557b44907d7cd5
Jared Elliott <bootcamp@jaredclt.com>
2024-03-05 21:40:59 -0500

    Fixed issues with curr users bounties.

--- client/src/components/myBounties.jsx
+++ client/src/components/myBounties.jsx
@@ -1,28 +1,27 @@
 import { useQuery } from '@apollo/client';
-import { QUERY_ME } from '../utils/queries';
+import { QUERY_MY_BOUNTIES } from '../utils/queries';
 
 export default function MyBounties() {
     
-    const { loading, data } = useQuery(QUERY_ME);
+    const { loading, data } = useQuery(QUERY_MY_BOUNTIES);
 
     if(loading) return <div className="container">Loading bounties, please wait...</div>
-    const user = data?.me || [];
+    const issues = data?.me.savedIssues || [];
+    console.log(issues)
 
     return (
         <div className="container" id="gh-repo-issues">
             <ul className="list-group">
-                {user.savedIssues.map((data, index) => 
+                {issues.map((issueData, index) => 
                     <li key={index} className="list-group-item d-flex align-items-center">
-                        <div className="badge text-bg-info rounded-pill">{data.vote}</div>
                         <div className="text-start mx-2">
-                            <h5 className="m-0">{data.title}</h5>
-                            <small>{data.description}</small>
+                            <h5 className="m-0">{issueData.title}</h5>
+                            <small>{issueData.description}</small>
                         </div>
-                        <button className="ms-auto btn btn-success" type="button">
-                            {data.bounty}
+                        <button className="ms-auto btn btn-success disabled" type="button">
+                            ${issueData.bounty/100}&nbsp;
                             <i className="fa-solid fa-hand-holding-dollar"></i>
                         </button>
-                        <a href="#" className="mx-2 btn btn-dark" title="Claim Bounty"><i className="fa-solid fa-virus-slash"></i></a>
                     </li>
                 )}
             </ul>
