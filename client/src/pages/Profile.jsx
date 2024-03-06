78f5aa0eb5b08f96e04cda07a419831ad876f17b
dcast217 <151418582+dcast217@users.noreply.github.com>
2024-03-05 19:55:02 -0500

    cleaning up

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -87,11 +87,6 @@ const Profile = () => {
                     <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
                         <div className="accordion-body">
                             <MyClaims />
-                            {/* <ul>
-                                <li>My Bounties claimed</li>
-                                <li>Will have to be listed here</li>
-                            </ul> */}
-                            {/* <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow. */}
                         </div>
                     </div>
                 </div>
