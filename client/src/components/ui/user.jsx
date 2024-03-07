a2235d6d2d1055e7bc5aa6e98f133e954839ae08
kyle coulter <152663440+coulterkyle@users.noreply.github.com>
2024-03-06 20:43:02 -0500

    github user fix

--- client/src/components/ui/user.jsx
+++ client/src/components/ui/user.jsx
@@ -3,20 +3,27 @@ import { Link } from "react-router-dom";
 export default function UserData({ users }) {
 
 
-    return (
-        <div className="container mb-4"  id="gh-user-list">
-        <ul className="list-group">
-          {users.map((data, index) => 
-            <li className="list-group-item d-flex align-items-center">
-                <div className="badge text-bg-info rounded-pill"></div>
-                <div className="text-start mx-2">
+  return (
+    <div className="container mb-4" id="gh-user-list">
+      <ul className="list-group">
+        {users.map((data, index) => {
+          {
+            if (data.githubUsername !== null) {
+              return (
+                <li className="list-group-item d-flex align-items-center">
+                  <div className="badge text-bg-info rounded-pill"></div>
+                  <div className="text-start mx-2">
                     <h5 className="m-0">{data.firstName} {data.lastName}</h5>
                     <small>{data.githubUsername}</small>
-                </div>
-                <Link key={index} to={data.githubUsername} className="ms-auto btn btn-success">View</Link>
-            </li>
-          )}
-        </ul>
-      </div>
-    );
+                  </div>
+                  <Link key={index} to={data.githubUsername} className="ms-auto btn btn-success">View</Link>
+                </li>
+              )
+            }
+          }
+        }
+        )}
+      </ul>
+    </div>
+  );
 }
